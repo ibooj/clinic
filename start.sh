09e72f4ca5187d909e7581e178ba207e5e3f5ddd
ibooj <ibooj@ya.ru>
2015-10-05 17:26:49 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt-get -y install python3 python3-dev python3-pip
 sudo pip3 install --upgrade -r requirements/dev.txt
-echo "\033[1mlogin: admin\033[0m";
-echo "\033[1mpassword: 123456\033[0m";
+echo -e "\033[1mlogin: admin\033[0m";
+echo -e "\033[1mpassword: 123456\033[0m";
 sudo python3 manage.py runserver 0.0.0.0:80
